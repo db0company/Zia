2fc805bee86fb0b81f77c3a3ba9acf188ebb1434
Frank LENORMAND <lenormf@gmail.com>
2012-03-05 17:09:49 +0100

    [API] Removed the buffer interface, improved the request one.

--- api/irequest.hpp
+++ api/irequest.hpp
@@ -7,33 +7,49 @@
 #define __IREQUEST_H__
 
 #include <list>
-#include <string>
 #include "httpdef.hpp"
 
 namespace zia { namespace http {
-	class IBuffer;
-
+	template <typename T>
 	class IRequest {
+		typedef T buffer_type;
+
 		public:
 		virtual ~IRequest() {}
 
 		// Get the buffer associated to the request
-		virtual IBuffer *getBuffer() const = 0;
-
-		// Assign a buffer to the request
-		virtual void setBuffer(IBuffer *buffer) = 0;
+		virtual buffer_type const &getBuffer() const = 0;
 
 		// A few simple getters for the HTTP request attributes
 		// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
 		virtual method getMethod() const = 0;
-		virtual std::string const &getURI() const = 0;
+		virtual buffer_type const &getURI() const = 0;
 		virtual version getVersion() const = 0;
 
 		// Get a particular entity-body, associated with a header name
-		virtual std::string const &getHeaderContent(char const *header) = 0;
-		virtual std::list<std::string> getHeaders() const = 0;
-		virtual std::list<std::string> getHeadersName() const = 0;
-		virtual std::list<std::string> getHeadersContent() const = 0;
+		virtual buffer_type getHeaderContent(buffer_type const &header_name) = 0;
+		virtual std::list<std::pair<buffer_type, buffer_type> > getHeaders() const = 0;
+		virtual std::list<buffer_type> getHeadersName() const = 0;
+		virtual std::list<buffer_type> getHeadersContent() const = 0;
+
+		// Assign a buffer to the request
+		virtual void setBuffer(buffer_type const &buffer) = 0;
+
+		// Setters in relation to the above getters
+		virtual void setMethod(method new_method) = 0;
+		virtual void setURI(buffer_type const &new_uri) = 0;
+		virtual void setVersion(version new_version) = 0;
+
+		// Modify the body of the request
+		virtual void setHeaderContent(buffer_type const &header_name,
+					      buffer_type const &content) = 0;
+
+		// Add a header to the request
+		virtual void addHeader(buffer_type const &header_name,
+				       buffer_type const &content) = 0;
+
+		// Remove a header from the request
+		virtual void removeHeader(buffer_type const &header_name) = 0;
 	};
 }}
 
