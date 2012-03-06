76b67746ee0e8806a2634fc061261e6f24635937
Frank LENORMAND <lenormf@gmail.com>
2012-03-06 09:54:01 +0100

    [API] Better comments for the irequest prototypes.

--- api/irequest.hpp
+++ api/irequest.hpp
@@ -21,23 +21,28 @@ namespace zia { namespace http {
 		virtual buffer_type const &getBuffer() const = 0;
 
 		// A few simple getters for the HTTP request attributes
-		// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
 		virtual method getMethod() const = 0;
 		virtual buffer_type const &getURI() const = 0;
 		virtual version getVersion() const = 0;
 
 		// Get a particular entity-body, associated with a header name
 		virtual buffer_type getHeaderContent(buffer_type const &header_name) = 0;
+		// Get a list of all the headers added to the request, along
+		// with their value
 		virtual std::list<std::pair<buffer_type, buffer_type> > getHeaders() const = 0;
+		// Get a list of all the headers added to the request
 		virtual std::list<buffer_type> getHeadersName() const = 0;
+		// Get a list of all the headers' values of the request
 		virtual std::list<buffer_type> getHeadersContent() const = 0;
 
-		// Assign a buffer to the request
+		// Assign a new buffer to the request
 		virtual void setBuffer(buffer_type const &buffer) = 0;
 
-		// Setters in relation to the above getters
+		// Set the method of the request
 		virtual void setMethod(method new_method) = 0;
+		// Set the URI of the request
 		virtual void setURI(buffer_type const &new_uri) = 0;
+		// Set the version of the HTTP request
 		virtual void setVersion(version new_version) = 0;
 
 		// Modify the body of the request
