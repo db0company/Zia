307fd72eb42b05dc843b0ca97a0afea4124b95d4
Becker <becker_g@epitech.eu>
2012-03-09 17:06:05 +0100

    [API] updated the IModule interface

--- api/IModule.h
+++ api/IModule.h
@@ -1,23 +1,28 @@
 #ifndef		IMODULE_H__
 # define	IMODULE_H__
 
-#include	<string>
+# include	<string>
 
-class		IModule
+# include	"dll.hpp"
+
+namespace zia
 {
-public :
-	virtual IModule&	operator=(const IModule&) = 0;
+	class		IModule
+	{
+	public :
+		virtual IModule&	operator=(const IModule&) = 0;
 
-
-/* ************************************************************************* */
-/*                             Member Functions                              */
-/* ************************************************************************* */
-	virtual void	load() = 0;
-	virtual void	loadMethod(const std::string&) = 0;
-	virtual void	unloadMethod(const std::string&) = 0;
-
-	virtual	bool	enable() = 0;
-	virtual	bool	disable() = 0;
-};
-
+	/* ************************************************************************* */
+	/*                             Member Functions                              */
+	/* ************************************************************************* */
+		virtual void	connection() = 0;
+		virtual void	deconnexion() = 0;
+		virtual void	load(const zia::filesystem::dll *) = 0;
+		virtual void	unload() = 0;
+		virtual void	loadMethod(const zia::http::method&) = 0;
+		virtual void	unloadMethod(const zia::http::method&) = 0;
+		virtual void	answer(const zia::http::method&) = 0;
+		virtual void	send() = 0;
+	};
+}
 #endif		//!IMODULE_H__
