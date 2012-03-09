9056814e4859635e8b40bf4d0ec9df80f0261d97
Becker <becker_g@epitech.eu>
2012-03-09 17:08:25 +0100

    [API] Deleted the ModuleManager and Adding the IModuleManager interface

---
+++ api/IModuleManager.h
@@ -0,0 +1,34 @@
+#ifndef		IMODULEMANAGER_H__
+# define	IMODULEMANAGER_H__
+
+# include	<list>
+# include	<string>
+
+# include	"dll.hpp"
+# include	"IModule.h"
+
+class IModuleManager
+{
+private:
+	virtual ModuleManager(const ModuleManager&) = 0;
+	virtual ModuleManager &	operator=(const ModuleManager&) = 0;
+	virtual ~ModuleManager(void);
+
+public:
+	virtual const IModuleManager * getInstance() = 0;
+
+	virtual const std::list<const IModule *> & getModules() const = 0;
+
+	virtual const IModule *	loadModule(const zia::filesystem::dll *) = 0;
+	virtual const IModule *	unloadModule(const IModule * &) = 0;
+
+	virtual std::list<const IModule *> connectModules(const std::list<IModule*>&) = 0;
+	virtual bool isconnected(const IModule*) = 0;
+	virtual std::list<const IModule *> disconnectModules(const std::list<IModule*>&) = 0;
+
+	virtual bool useMethod(const zia::http::method&) = 0;
+	virtual bool execMethod(const zia::http::IRequest&) = 0;
+	virtual const IRequest& getAnswer() = 0;
+};
+
+#endif		//! MODULEMANAGER_H__
