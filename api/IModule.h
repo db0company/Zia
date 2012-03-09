b075c54a039d116c5947e259a6289cd61e6801b4
Becker <becker_g@epitech.eu>
2012-03-09 21:45:17 +0100

    [API] rectification of IModule.h and IModuleManager.h

--- api/IModule.h
+++ api/IModule.h
@@ -15,14 +15,12 @@ namespace zia
 	class		IModule
 	{
 	public :
-		virtual IModule&	operator=(const IModule&) = 0;
-
 	/* ************************************************************************* */
 	/*                             Member Functions                              */
 	/* ************************************************************************* */
 		virtual void	connection() = 0;
 		virtual void	deconnexion() = 0;
-		virtual void	load(const zia::filesystem::dll::AHandler *) = 0;
+		virtual void	load(zia::filesystem::dll::AHandler *) = 0;
 		virtual void	unload() = 0;
 		virtual void	loadMethod(const zia::http::method&) = 0;
 		virtual void	unloadMethod(const zia::http::method&) = 0;
--- api/IModuleManager.h
+++ api/IModuleManager.h
@@ -21,16 +21,16 @@ namespace zia
 
 		virtual const std::list<const IModule *> & getModules() const = 0;
 
-		virtual const IModule *	loadModule(const zia::filesystem::dll::AHandler *) = 0;
+		virtual const IModule *	loadModule(zia::filesystem::dll::AHandler *) = 0;
 		virtual const IModule *	unloadModule(const IModule *) = 0;
 
-		virtual std::list<const IModule *> connectModules(const std::list<IModule *>&) = 0;
-		virtual bool isconnected(const IModule *) = 0;
-		virtual std::list<const IModule *> disconnectModules(const std::list<IModule *>&) = 0;
+		virtual const std::list<const IModule *> connectModules(const std::list<IModule *>&) = 0;
+		virtual bool isConnected(const IModule *) = 0;
+		virtual const std::list<const IModule *> disconnectModules(const std::list<IModule *>&) = 0;
 
 		virtual bool useMethod(const zia::http::method&) = 0;
 		virtual bool execMethod(const zia::http::IRequest *) = 0;
-		virtual const IRequest& getAnswer() = 0;
+		virtual const IRequest * getAnswer() = 0;
 	};
 }
 #endif		//! MODULEMANAGER_H__
