96f89d618560083c66f8e148c50a40732d49b09e
Frank LENORMAND <lenormf@gmail.com>
2012-04-06 13:45:24 +0200

    [HTTP] Implemented the API Request/Response objects, added a short test file

---
+++ src/http/test.cpp
@@ -0,0 +1,42 @@
+/*
+ * test.cpp for Zia
+ * by lenorm_f
+ */
+
+// g++ -Wall -Wextra -I../../bref-api/include/bref test.cpp HttpRequest.cpp HttpResponse.cpp
+
+#include <iostream>
+#include <HttpRequest.h>
+#include <HttpResponse.h>
+
+static std::string to_string(bref::Buffer buffer) {
+	std::string s;
+
+	bref::Buffer::const_iterator it;
+	for (it = buffer.begin(); it != buffer.end(); ++it)
+		s += *it;
+
+	return s;
+}
+
+int main() {
+	bref::HttpRequest req;
+	bref::HttpResponse rep;
+
+	req.setVersion(bref::Version(1, 1));
+	req.setUri("/index.php");
+	req.setMethod(bref::request_methods::Get);
+
+	std::cout << "Request: " << static_cast<int>(req.getMethod())
+		<< " " << req.getUri()
+		<< " HTTP/" << req.getVersion().Major
+		<< "." << req.getVersion().Minor
+		<< std::endl;
+
+	rep.setVersion(bref::Version(1, 1));
+	rep.setStatus(bref::status_codes::OK);
+	rep.setReason("Oui.");
+
+	std::cout << "Response: " << to_string(rep.getRawData())
+		<< std::endl;
+}
